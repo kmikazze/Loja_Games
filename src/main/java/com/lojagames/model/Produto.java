9a3750555f57fdf3f10449d53524e79a82f3f51d
unknown <you@example.com>
2023-07-03 17:11:36 -0300

    Projeto - lojaGames

---
+++ src/main/java/com/lojagames/model/Produto.java
@@ -0,0 +1,96 @@
+package com.lojagames.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O título é obrigatório!")
+	@Size(min = 5, max = 10000, message = "O título deve conter no mínimo 05 e no máximo 10000 caracteres.")
+	private String nomeJogo;
+	
+	@NotBlank
+	private String anoLancamento;
+	
+	@NotNull
+	@Size(min = 3, max = 100)
+	private String aparelho;
+	
+	@NotNull(message = "Preço é obrigatório!")
+    @PositiveOrZero(message = "O preço deve ser maior do que zero!") //caso o valor seja 0, pode-se usar @
+    private BigDecimal preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeJogo() {
+		return nomeJogo;
+	}
+
+	public void setNomeJogo(String nomeJogo) {
+		this.nomeJogo = nomeJogo;
+	}
+
+	public String getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(String anoLancamento) {
+		this.anoLancamento = anoLancamento;
+	}
+
+	public String getAparelho() {
+		return aparelho;
+	}
+
+	public void setAparelho(String aparelho) {
+		this.aparelho = aparelho;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+}
